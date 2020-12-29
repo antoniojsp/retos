3d9bd5fd40edeaefdd2685961b5e10f2d75a097c
Antonio Silva Paucar <antonios@uoregon.edu>
2020-12-29 00:27:02 -0800

    hourglass

---
+++ hourglass.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    maximo = float('-inf')
+    for i in range(1,len(arr)-1):
+        for j in range(1,len(arr)-1):
+            maximo = max(maximo, arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1])
+
+    return maximo
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
