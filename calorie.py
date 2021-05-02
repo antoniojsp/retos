e281b079c63f038a0ad1fe580086f04ae3d74e6c
antonio Silva <>
2021-05-02 04:06:14 -0700

    calorie

---
+++ calorie.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/marcs-cakewalk/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    temp = sorted(calorie, reverse=True)
+    suma = 0
+    for i in range(len(temp)):
+        suma += 2**i*temp[i]
+    return suma
+ 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
