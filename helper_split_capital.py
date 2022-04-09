72ed80b0d2c8bb37b3355c76d6e4bc8512355cea
antonio Silva <antonios@uoregon.edu>
2022-04-09 11:15:46 -0700

    casi listo

--- helper_split_capital.py
+++ helper_split_capital.py
@@ -8,12 +8,12 @@ def split_capital(string:str) -> list:
     result = []  # holds the results
     temp = ""  # holds one section of the string being processed
     for index, variable in enumerate(string):
-        if variable.isupper() and index != 0:
+        if variable.isupper() and index != 0:  # if char is capital and not the first.
             result.append(temp)
             temp = ''
         temp += variable
 
-        if variable == string[-1]: # checks for the end of the string and add the last part
+        if variable == string[-1]:  # checks for the end of the string and add the last part
             result.append(temp)
     return result
 
