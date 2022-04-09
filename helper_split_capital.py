16e446c5395f1c1a320ead842a5ac337f28db2b7
antonio Silva <antonios@uoregon.edu>
2022-04-09 11:06:26 -0700

    desd pycharm

--- helper_split_capital.py
+++ helper_split_capital.py
@@ -1,24 +1,22 @@
 a = "LargeSoftwareBook"
 
-def split_capital(str:str)->list:
 
+def split_capital(string:str) -> list:
+    """
+    (str)->[]
+    """
     result = []
-    temp = "" #holds one section of the string being processed
-
-    # for i in range(1,len(str)):
-
-    for i in enumerate(str):
-
+    temp = ""  # holds one section of the string being processed
+    for i in enumerate(string):
         if i[1].isupper() and i[0] != 0:
             result.append(temp)
             temp = ''
-        temp+= i[1]
+        temp += i[1]
 
         if i[0] == len(str)-1:
             result.append(temp)
-
-
     return result
+
 
 print(split_capital(a))
 
