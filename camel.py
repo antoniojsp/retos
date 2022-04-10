79cb9b7bc15aa2cfa742a36ba630c01304c6c16a
Antonio Silva Paucar <antonios@uoregon.edu>
2022-04-10 01:06:41 -0700

    aa

--- camel.py
+++ camel.py
@@ -1,7 +1,5 @@
-https://www.hackerrank.com/challenges/three-month-preparation-kit-camel-case/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
-
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-def split_capital(string:str) -> list:
+def split_capital(string: str) -> list:
     """
     (str)->[]
     """
@@ -13,23 +11,47 @@ def split_capital(string:str) -> list:
             temp = ''
         temp += variable
 
-        if variable == string[-1]:  # checks for the end of the string and add the last part
-            result.append(temp)
+        if index == len(string) - 1:  # checks for the end of the string and add the last part
+            result.append(temp.rstrip())
+    # print(result)
     return result
 
+
 while True:
-    linea = input().split(";")
-    print(linea)
-    if linea[0] == "C":
-        option = linea[1]
-        if option in ["M", "V"] :
-            temp = linea[2].split()
-            for i in range(0, len(temp)):
-                if i == 0:
-                    print(temp[i], end="")
-                else:
-                    print(temp[i].capitalize(), end="")
-            print("()")
-        else:
-            temp = linea[2].split()
-            print("{}{}".format(temp[0].capitalize(),temp[1].capitalize()))
+    try:
+        linea = input().split(";")
+        # print(linea)
+        result = []
+        if linea[0] == "C":  # combine
+            option = linea[1]
+            if option in ["M", "V"]:
+                temp = linea[2].split()
+                # temp_result = ""
+                for i in range(0, len(temp)):
+                    if i == 0:
+                        print(temp[i], end="")
+                        # temp_result += temp[i]
+                    else:
+                        print(temp[i].capitalize(), end="")
+                        # temp_result += temp[i].capitalize()
+                if option == "M":
+                    print("()")
+
+            else:
+                temp = linea[2].split()
+                print("{}{}".format(temp[0].capitalize(), temp[1].capitalize()))
+
+        else:  # split
+            option = linea[1]
+            if option in ["C", "V"]:
+                temp = split_capital(linea[2])
+                for i in temp:
+                    print(i.lower(), end=" ")
+                print()
+            else:
+                print(linea[2])
+    except:
+        break
+
+
+
