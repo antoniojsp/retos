8dddf18e48d922e575241bbb12227f8b3b53a7a0
antonio Silva <antonios@uoregon.edu>
2022-04-12 18:14:29 -0700

    string queries

--- pair.py
+++ pair.py
@@ -1,17 +1,25 @@
-multiples = {}
-
-k = 4
-maximum = 200
-for i in range(1, 6):
-    if i*k > maximum:
-        break
-    multiples[k*i] = 0
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+k = 3
+maximum = 10
+multiples = [i*k for i in range(1, maximum) if i*k <= maximum]
+arr = [1, 3, 2, 6, 1, 2]
+dict_arr = {i: 0 for i in arr}
+print(dict_arr)
+print(multiples)
 # c = {4: 0, 8: 0, 12: 0, 16: 0, 20: 0}
 # a + b = c
 # c % k = 0
 # a = c - b
 
-print(multiples)
-
+# 4 = 1 + 1
+# 4 = 1 + 2
+# 4 = 1 + 3
+# 4 = 1 + 4
+value = 0
+for i in sorted(arr):
+    print(i, "value")
+    for j in multiples:
+        temp = abs(j - i)
+        if temp in dict_arr:
+            if temp != i and temp < i:
+                value += 1
+                print(str(temp) + " + " + str(i) + " = " + str(j))
