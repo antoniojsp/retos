f557c4bc0a1161f42a5eb39ec2a3f694d2e54d5f
antonio Silva <antonios@uoregon.edu>
2022-04-13 20:25:00 -0700

    sos

---
+++ sos.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-mars-exploration/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    mask = "SOS" * int(len(s) / 3)
+    count = 0
+    for i, j in zip(s, mask):
+        if i != j:
+            count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
