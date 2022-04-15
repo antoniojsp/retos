0e5c953c6a879fd407acf2353f655e9ad5b07c4d
antonio Silva <antonios@uoregon.edu>
2022-04-15 06:40:47 -0700

    nuev

---
+++ migratory_bird.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-migratory-birds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    temp = Counter(arr)
+    maximo_freq = max(temp.values())
+    ids = []
+    for i, j in temp.items():
+        if j == maximo_freq:
+            ids.append(i)
+    return min(ids)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
