2dac25509fe009e4b50ff817bb6e44984042fbb8
antonio Silva <antonios@uoregon.edu>
2022-04-16 22:38:07 -0700

    elevation

---
+++ hike_elevation.py
@@ -0,0 +1,44 @@
+#https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valley, altitude = 0, 0
+    for i in path:
+        if i == "U":
+            altitude += 1
+            valley = valley + 1 if altitude == 0 else valley
+        else:
+            altitude += -1
+
+    return valley
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
