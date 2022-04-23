47ae2b97a3b73a428c43e463fe796bc20d7955b5
antonio Silva <antonios@uoregon.edu>
2022-04-22 18:31:28 -0700

    chocolate bar sum of n squares

---
+++ left_rotation.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-array-left-rotation/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-four
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    result = []
+    i = d
+    while len(result) != len(arr):
+        result.append(arr[i])
+        i += 1
+        if i > len(arr) - 1:
+            i = 0
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
