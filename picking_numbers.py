76e649f0f2d3c9d44938719cd2a40218d5f5cef3
antonio Silva <antonios@uoregon.edu>
2022-04-26 22:40:14 -0700

    nuevo funcion

--- picking_numbers.py
+++ picking_numbers.py
@@ -20,51 +20,48 @@ def isAbsoluteValueDiffRight(first_value, second_value):
 
 def pickingNumbers(a):
     # Write your code here
+    a.sort()
+    dictionary_values = {}
+    for valueArr in a:
+        if valueArr in dictionary_values:
+            dictionary_values[valueArr] += 1
+        else:
+            dictionary_values[valueArr] = 1
 
-    # using a dictionary to count the incidences of values in the array
-    # a.sort()
-    # dictionary_values = {}
+    uniqueValues = list(dictionary_values)
 
-    # for i in a:
-    #     if i in dictionary_values:
-    #         dictionary_values[i] += 1
-    #     else:
-    #         dictionary_values[i] = 1
+    if len(uniqueValues) == 1:  # Case array is filled up with the same unique value
+        return dictionary_values[uniqueValues[0]]
 
-    # # print(dictionary_values)
-    # keys = list(dictionary_values)
-    # i = 0
-    # result = []
+    maxDiffSoFar = 0
+    i = 0
+    while i < len(dictionary_values) - 1:
+        current_value = uniqueValues[i]
+        next_value = uniqueValues[i + 1]
+        if isAbsoluteValueDiffRight(current_value, next_value):
+            maxDiffSoFar = max(maxDiffSoFar, dictionary_values[current_value] + dictionary_values[next_value])
+            i += 2
+        else:
+            maxDiffSoFar = max(maxDiffSoFar, dictionary_values[current_value])
+            i += 1
 
-    # if len(keys) == 1: # if the array is filled up with the same unique value
-    #     return dictionary_values[keys[0]]
-
-    # maximo = 0
-    # while i < len(keys)-1:
-    #     if abs(keys[i] - keys[i+1]) == 1:
-    #         maximo = max(maximo, dictionary_values[keys[i]] + dictionary_values[keys[i+1]])
-    #         i+=2
-    #     else:
-    #         maximo = max(maximo, dictionary_values[keys[i]])
-    #         i+=1
-
-    # return maximo
+    return maxDiffSoFar
 
     # version using list.
-    a.sort()
-    current_value = a[0]
-    count = 0
-    maximoSoFar = 0
-    for value in a:
-        if isAbsoluteValueDiffRight(value, current_value):
-            count += 1
-            maximoSoFar = max(maximoSoFar, count)
-        else:
-            maximoSoFar = max(maximoSoFar, count)
-            current_value = value
-            count = 1
+    # a.sort()
+    # current_value = a[0]
+    # count = 0
+    # maximoSoFar = 0
+    # for value in a:
+    #     if isAbsoluteValueDiffRight(value, current_value):
+    #         count += 1
+    #         maximoSoFar = max(maximoSoFar, count)
+    #     else:
+    #         maximoSoFar = max(maximoSoFar, count)
+    #         current_value = value
+    #         count = 1
 
-    return maximoSoFar
+    # return maximoSoFar
 
 
 if __name__ == '__main__':
