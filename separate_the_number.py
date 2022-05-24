93f93bcfb6b3a0582050373ac16adb2674f1f464
antonio Silva <antonios@uoregon.edu>
2022-05-23 20:12:29 -0700

    sherlock and recursive

--- separate_the_number.py
+++ separate_the_number.py
@@ -1,17 +1,12 @@
 #https://www.hackerrank.com/challenges/three-month-preparation-kit-separate-the-numbers/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-four
 from math import floor
+
+
+def separate_numbers(s):
+
+    first_occurrence = s[:1]
+    print(first_occurrence)
+
 s = "1234"
-
-def separateNumbers(s):
-
-    lenght = len(s)
-    for i in range(0, floor(len(s)/2)):
-        current = s[:i]
-        for j in range(1, floor(len(s)/2)):
-            print(current)
-            print((s[i:j]))
-
-        print(current)
-
-
-separateNumbers(s)
+example2 = "91011"
+separate_numbers(s)
