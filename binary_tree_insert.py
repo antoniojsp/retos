9b4be86b663a71e44e21ab6de217a1b388f21828
antonio Silva <antonios@uoregon.edu>
2023-01-31 19:44:16 -0800

    linked list head

---
+++ binary_tree_insert.py
@@ -0,0 +1,55 @@
+# https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem?isFullScreen=true
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+def preOrder(root):
+    if root == None:
+        return
+    print(root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # Node is defined as
+    # self.left (the left child of the node)
+    # self.right (the right child of the node)
+    # self.info (the value of the node)
+
+    def insert(self, val):
+        # Enter you code here.
+        new_node = Node(val)
+        if self.root is None:
+            self.root = new_node
+            return
+
+        current = self.root
+
+        while True:
+            if current.info > val:
+                if current.left is None:
+                    current.left = new_node
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = new_node
+                    break
+                current = current.right
+
+        return self.root
+
+
+tree = BinarySearchTree()
