b7c5b7bd5b54b58698f741e30e48a509e1c636d4
Antonio Silva Paucar <antonios@uoregon.edu>
2023-03-20 18:21:53 -0700

    desde la pc, remover error

--- climbing_the_leaderboard.py
+++ climbing_the_leaderboard.py
@@ -6,13 +6,28 @@
 ranking = [100, 100, 50, 40, 40, 20, 10]
 player = [5, 25, 50, 120]
 
+
+def binary_search(arr: list, val):
+    start = 0
+    middle = len(arr) // 2
+    end = len(arr) - 1
+    while True:
+        if arr[middle] == val:
+            return middle
+        if arr[middle] < val:
+            end = middle - 1
+            middle = (start + end) // 2
+        if arr[middle] > val:
+            start = middle + 1
+            middle = (start + end) // 2
+
 def climbingLeaderboard(ranked, player):
+    result = []
+    for score in player:
+        temp = ranked + [score]
+        rank = sorted(set(temp), reverse=True)
+        result.append(binary_search(rank, score)+1)
 
-    for i in player:
-        temp = ranked + [i]
-        temp.sort()
-        new = set(temp)
-        sorted_list = list(new)[::-1]
-        print(sorted_list)
+    return result
 
-climbingLeaderboard(ranking, player)
+print(climbingLeaderboard(ranking, player))
