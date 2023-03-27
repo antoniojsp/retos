cc1d22421dc6dcd7f6cf8114f02254c277930b68
antonio Silva <antonios@uoregon.edu>
2023-03-27 15:53:05 -0700

    linked list head

--- sherlock_valid_string.py
+++ sherlock_valid_string.py
@@ -1,12 +1,33 @@
-'''
-rules:
-valid string
-- all chars appears same number of time.
-- if you can remove char at index 1 and number of character is same
-'''
-
 from collections import Counter
 
-test = ['aabbccddeefghi', 'abcdefghhgfedecba','aabbcd']
+
+def isValid(s):
+    count = list(Counter(s).values())
+    #case 1 frequency all char are equal
+    temp = set(count)
+    print(count)
+    print(temp)
+    if len(temp) == 1:
+        return "YES"
+    #case 2 more than 2 frequancies
+    if len(temp) > 2:
+        return "NO"
+    list_set = list(temp)
+
+    if count.count(1) > 1 :
+        return "NO"
+
+    if abs(list_set[0]-list_set[1]) > 1:
+        return "NO"
+
+    return "YES"
+
+
+
+test = ["aabbcd"]
+
 for i in test:
-    print(Counter(i))
+    print(isValid(i))
+
+
+
