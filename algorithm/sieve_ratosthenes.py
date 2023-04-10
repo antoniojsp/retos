b47b17878a9be84d29f8c8cfbedffe59b8e21edf
antonio Silva <antonios@uoregon.edu>
2023-04-09 18:23:57 -0700

    Sherlock valid string final, pass all

--- algorithm/sieve_ratosthenes.py
+++ algorithm/sieve_ratosthenes.py
@@ -1,16 +1,13 @@
 
-from math import sqrt, ceil, floor
+from math import sqrt, ceil
 def sieve(n):
     integers = [True for _ in range(0, n+1)]
     integers[0], integers[1] = False, False
-    count = 0
     for i in range(ceil(sqrt(len(integers)))):
         if integers[i]:
             for j in range(i*i, len(integers), i):
-                count +=1
                 integers[j] = False
 
-
-    return [i for i in range(len(integers)) if integers[i]]
+    return [index for index, is_prime in enumerate(integers) if is_prime]
 
 print(sieve(1000))
