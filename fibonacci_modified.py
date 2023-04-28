8a5154d5eb1cb2e60b05bb7e8e6129244297b8eb
antonio Silva <antonios@uoregon.edu>
2023-04-28 12:08:50 -0700

    delete empty files

--- fibonacci_modified.py
+++ fibonacci_modified.py
@@ -1,10 +1,11 @@
 
 def fibonacciModified(t1, t2, n):
     # Write your code here
+    next_t = 0
     for i in range(2, n):
-        temp = t1 + t2**2
-        t1, t2 = t2, temp
-    return temp
+        next_t = t1 + t2**2
+        t1, t2 = t2, next_t
+    return next_t
 
 
-print(fib(5))
+print(fibonacciModified(0,1 ,5))
