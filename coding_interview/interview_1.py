1502684cdcf32d51acac64ce249c300234b8d868
antonio Silva <antonios@uoregon.edu>
2023-05-03 22:52:52 -0700

    first

--- coding_interview/interview_1.py
+++ coding_interview/interview_1.py
@@ -4,21 +4,35 @@ import random
 class Test:
     def __init__(self):
         # self.storage = [] time consuming
-        self.storage = {}
+        self.storage = []
         self.map = {}
+        self.length = 0
 
     def insert(self, val):
         if val in self.storage:
             print("Duplicate")
             return 0
+        self.storage.append(val)
+        self.map[val] = self.length
+        self.length += 1
 
-        self.storage[val] = 0
 
     def remove(self, val):
-        del self.storage[val]
+        temp = self.storage.pop()
+        if temp == val:
+            return
+
+        old_index = self.map[val]
+        self.storage[old_index] = temp
+        self.map[temp] = old_index
+        self.length -=1
+
+
 
     def random(self):
-        return random.choice(list(self.storage.keys()))
+        index = random.randint(0, self.length-1)
+        print("index",index)
+        return self.storage[index]
 
     def __repr__(self):
         return str(self.storage)
@@ -31,7 +45,10 @@ a.insert(3)
 a.insert(1)
 print(a)
 print(a.random())
-
+#
 a.remove(3)
 print(a)
+a.insert((7))
+print(a)
+
 print(a.random())
