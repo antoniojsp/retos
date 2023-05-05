8fb999d4218175bbf7bdf4a1a44cee278939dc77
antonio Silva <antonios@uoregon.edu>
2023-05-05 14:20:33 -0700

    first

--- coding_interview/product_array_except_ith.py
+++ coding_interview/product_array_except_ith.py
@@ -11,7 +11,9 @@ def multiply_array(arr:list)->int:
 
     return total_multiplication
 def product_array(arr:list)->list:
-    #case one zero
+    '''
+    Works for positive, negative and when there is only one zero.
+    '''
     multiple = multiply_array(arr)
     if 0 in arr:
         result = [0 if i != 0  else multiple for i in arr]
@@ -19,7 +21,45 @@ def product_array(arr:list)->list:
         result = [int(multiple/i) for i in arr]
     return result
 
+array = [1,0,0,2]
+# [0, 0, 0, 0]
+from functools import reduce
+def product_array_zeros_brute(arr:list)->list:
+    #brute force
+    mul = lambda x, y: x*y
+    result = []
+    for i in range(0, len(arr)):
+        arr_all_but_current = arr[0:i]+ arr[i+1:len(arr)]
+        result.append(reduce(mul, arr_all_but_current))
 
-print(product_array([1,2,0,0]))
+    return result
 
-#what if array [1,2,0,4]
+def product_array_zeros(arr:list)->list:
+
+    left = arr.copy()
+    for i in range(1, len(left)):
+        left[i] = left[i-1]*left[i]
+
+    right = arr.copy()
+    right.reverse()
+    for i in range(1, len(right)):
+        right[i] = right[i-1]*right[i]
+    right.reverse()
+
+    result = []
+    for i in range(len(arr)):
+        if i == 0:
+            result.append(1*right[i+1])
+
+        elif i == len(arr)-1:
+            result.append(1*left[i-1])
+        else:
+            result.append(left[i-1]*right[i+1])
+
+    return result
+
+
+array = [0,1,1,2]
+print(product_array_zeros(array))
+
+
