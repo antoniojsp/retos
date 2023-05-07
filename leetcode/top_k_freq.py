e03ab0dfdf27fc7149955aec7ae4dcc471be703e
antonio Silva <antonios@uoregon.edu>
2023-05-07 11:32:18 -0700

    final array product

--- leetcode/top_k_freq.py
+++ leetcode/top_k_freq.py
@@ -3,8 +3,10 @@ from collections import Counter
 def topKFrequent(nums, k):
     count = Counter(nums)
     value = list(count.items())
-    sort_dict = lambda data : data[]
+    sort_dict = lambda data : (data[0],data[1])
+    a = value.sort(sort_dict)
 
 nums = [1,1,1,2,2,3]
 k = 2
 print(topKFrequent(nums, k))
+
