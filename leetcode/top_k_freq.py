f74cd77c964d4df0522446ddcf88fefb460f0077
antonio Silva <antonios@uoregon.edu>
2023-05-08 02:19:08 -0700

    final array product

--- leetcode/top_k_freq.py
+++ leetcode/top_k_freq.py
@@ -1,15 +1,18 @@
+#https://leetcode.com/problems/top-k-frequent-elements/description/
+
 from collections import Counter
+
+# binary = lambda x: format(1<<x, '08b')
 
 def topKFrequent(nums, k):
     count = dict(Counter(nums))
-    # value = list(count.items())
-    print(count)
-    result = sorted(count.items(), key=lambda x:x[0])[:k]
-    print(result)
+    sort_dict = lambda x: x[1]
+    ordered = list(sorted(count.items(), key=sort_dict, reverse=True))[:k]
+    return [i[0] for i in ordered]
 
-    return [i[1] for i in result]
 
-nums = [1,1,1,2,2,3]
-k = 2
+
+
+nums = [-1,-1]
+k = 1
 print(topKFrequent(nums, k))
-
