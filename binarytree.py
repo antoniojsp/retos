1a3c0c6b6c1117d5cc88ac8aa03f329ac0184fb9
antonio Silva <antonios@uoregon.edu>
2023-05-21 02:52:26 -0700

    three sum

--- binarytree.py
+++ binarytree.py
@@ -49,20 +49,56 @@ class BST:
 
         return helper(root)
 
+    def maximum(self):
+        root = self.head
+        def helper(node):
+            if not node.right:
+                return node.data
+
+            return helper(node.right)
+
+        return helper(root)
+
+    def search(self, looking_for):
+        root = self.head
+
+        def helper(node, val):
+            print("value", val)
+            if not node:
+                return
+            print(node.data)
+            if node.data == val:
+                return True
+
+            elif val < node.data:
+                helper(node.left, val)
+            else:
+                helper(node.right, val)
+
+            return False
+
+        return helper(root, looking_for)
+
+
+
+
 
 
 
 
 a = BST()
-a.insert(10)
+a.insert(30)
 a.insert(5)
 a.insert(10)
-a.insert(3)
+a.insert(35)
 a.insert(5)
 a.insert(9)
 a.insert(14)
 a.insert(1)
 
-a.print()
-print(a.minimum())
+# a.print()
+print(a.maximum())
+print(a.search(9))
+# print(a.search(9000))
+
 
