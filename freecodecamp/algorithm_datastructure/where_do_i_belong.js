a00b1826d7f8ddbbff40333176fa4f1c9bb99d25
antonio Silva <antonios@uoregon.edu>
2023-07-10 08:54:52 -0700

    freecodecamp

---
+++ freecodecamp/algorithm_datastructure/where_do_i_belong.js
@@ -0,0 +1,16 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/where-do-i-belong
+
+function getIndexToIns(arr, num) {
+  arr.sort((a,b)=>a-b)
+  let i = 0;
+  while (i < arr.length){
+    if (arr[i]>=num){
+      break;
+    };
+    i++;
+  };
+  return i;
+};
+
+// getIndexToIns([40, 60], 50);
+console.log(getIndexToIns([5, 3, 20, 3], 5) )
