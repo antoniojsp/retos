89a2202cf893b574d0899e93af0facd322e3ae55
Antonio Silva Paucar <antonios@uoregon.edu>
2023-10-21 15:42:50 -0700

    longest_prefix

--- leetcode/power_of_three_326.py
+++ leetcode/power_of_three_326.py
@@ -3,13 +3,14 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n < 1:
             return 0
-        result = log(n, 3)
-        f, c = floor(result), ceil(result)
-        print(result)
-        return abs(f-result)<0.0001 or abs(c-result)<0.0001
+        # result = log(n, 3)
+        # f, c = floor(result), ceil(result)
+        # print(result)
+        # return abs(f-result)<0.0001 or abs(c-result)<0.0001
 
+        while n > 1 and n%3==0:
+            n//=3
 
+        return n == 1
 
-
-print(Solution().isPowerOfThree(19682))
 
