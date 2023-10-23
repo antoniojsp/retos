de7ab99c2c28c238bdef0f8d132e20094fa7f4e0
antonio Silva <antonios@uoregon.edu>
2023-10-23 02:54:26 -0700

    freecodecamp

--- leetcode/happy_number_202.py
+++ leetcode/happy_number_202.py
@@ -2,22 +2,29 @@ class Solution:
     def isHappy(self, n: int) -> bool:
 
         i = 0
-        while n > 0:
-            temp = []
-            while n > 0:
-                digit = n%10
-                n = n - digit
-                n//=10
-                temp.append(digit)
-
+        seen = set()
+        '''
+        To detect a loop, we need to find repeated work. if a solutiuon gives us a number we have
+        already seen, then it means it will loop forever. we set a "seen" set where we storage all
+        the values we have procesed to find repeated work, if we do, we return false. 
+        If we get a result of 1, then we return true.
+        '''
+        while n > 1:
             result = 0
-            for i in temp:
-                result+= i**2
+            # while n > 0:
+            #     digit = n % 10
+            #     n = n - digit
+            #     n //= 10
+            #     result += digit ** 2
+            value = str(n)
+            for i in value:
+                result += int(i)**2
 
+            if result in seen:
+                return False
+
+            seen.add(result)
             n = result
 
 
-
-
-a = Solution().isHappy(19)
-print(a)
+        return n == 1
