74ab8b590b4960ce4dd056367aa3e25185dac1c3
Antonio Silva Paucar <antonios@uoregon.edu>
2023-10-26 14:19:58 -0700

    longest_prefix

--- leetcode/excel_sheet_column_number_171.py
+++ leetcode/excel_sheet_column_number_171.py
@@ -1,14 +1,3 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-<<<<<<< HEAD
-
-
-
-
-
-columnTitle = "ZY"
-=======
-        print(columnTitle)
-
-        return 1
->>>>>>> origin/master
+        ...
