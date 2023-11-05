dc4209217d59a29a732893b4d5288af5780160c9
antonio Silva <antonios@uoregon.edu>
2023-11-05 01:35:42 -0800

    freecodecamp

--- leetcode/fibonacci_number_509.py
+++ leetcode/fibonacci_number_509.py
@@ -1,7 +1,22 @@
 class Solution:
-    def __init__(self):
-        self.cache= {}
-    def fib(self, n: int) -> int:
+
+    def fib(self, n:int, cache=None) -> int:
+
+        if not cache:
+            cache = {0: 0, 1: 1}
+        if n in cache:
+            return cache[n]
+        if n <= 1:
+            return n
+
+        cache[n] =  self.fib(n-1, cache) + self.fib(n-2, cache)
+
+        return cache[n]
+
+
+    def fib2(self, n: int) -> int:
+        if n == 0:
+            return 0
         result= [-1]*(n+1)
         result[0] = 0
         result[1] = 1
@@ -9,8 +24,11 @@ class Solution:
         for i in range(2, n+1):
 
             result[i] = result[i-1] + result[i-2]
-        print(result)
         return result[n]
 
 
-print(Solution().fib(3))
+temp = Solution()
+
+for i in range(100):
+    print(temp.fib(i))
+    assert temp.fib(i) == temp.fib2(i)
