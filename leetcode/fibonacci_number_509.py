6112bd9ca28a32c347e2284aef2619fc51519093
Antonio Silva Paucar <antonios@uoregon.edu>
2023-11-05 16:27:41 -0800

    weighted uniform

--- leetcode/fibonacci_number_509.py
+++ leetcode/fibonacci_number_509.py
@@ -1,34 +1,13 @@
 class Solution:
 
-    def fib(self, n:int, cache=None) -> int:
-
-        if not cache:
-            cache = {0: 0, 1: 1}
-        if n in cache:
-            return cache[n]
-        if n <= 1:
-            return n
-
-        cache[n] =  self.fib(n-1, cache) + self.fib(n-2, cache)
-
-        return cache[n]
-
-
-    def fib2(self, n: int) -> int:
+    def fib(self, n: int) -> int:
         if n == 0:
             return 0
-        result= [-1]*(n+1)
-        result[0] = 0
-        result[1] = 1
-
+        a, b  = 0, 1
         for i in range(2, n+1):
+            a = a + b
+            a, b = b, a
+        return b
 
-            result[i] = result[i-1] + result[i-2]
-        return result[n]
 
-
-temp = Solution()
-
-for i in range(100):
-    print(temp.fib(i))
-    assert temp.fib(i) == temp.fib2(i)
+print(Solution().fib(3))
