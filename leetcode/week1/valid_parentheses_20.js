4b65f64c2fe725f6694889337b7b3daa0a2c919a
antonio Silva <antonios@uoregon.edu>
2023-11-28 23:32:08 -0800

    parentheses

--- leetcode/week1/valid_parentheses_20.js
+++ leetcode/week1/valid_parentheses_20.js
@@ -1,5 +1,35 @@
 class Stack{
 
-    
+    constructor(){
+        this.arr = [];
+    };
 
-}
+    push(value){
+        this.arr.push(value);
+    }
+    peek(){
+        return this.arr[this.arr.length-1];
+    }
+    pop(){
+        return this.arr.pop();
+    }
+
+    isEmpty(){
+        return this.arr.length > 0;
+    }
+
+    print(){
+        console.log(this.arr)
+    }
+};
+
+
+function isValid(s){
+    store = new Stack()
+    for (i of s){
+
+    };
+
+};
+// {[{}]}
+isValid("antonio")
