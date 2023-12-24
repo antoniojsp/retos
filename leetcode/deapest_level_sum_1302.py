560e116719cf7591659124f917451313f486bfb1
antonio Silva <antonios@uoregon.edu>
2023-12-24 02:47:22 -0500

    recursion

---
+++ leetcode/deapest_level_sum_1302.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+
+        Q = deque([(root, 0)])  # creates a deque. Faster than using a list to pop first item.
+        max_level = 0
+        sum_level = {0: root.val}  #
+        while Q:
+            node, level = Q.popleft()
+            if level + 1 not in sum_level:
+                sum_level[level + 1] = 0
+            if node.left:
+                left = node.left
+                Q.append((left, level + 1))
+                sum_level[level + 1] += left.val
+            if node.right:
+                right = node.right
+                Q.append((right, level + 1))
+                sum_level[level + 1] += right.val
+            max_level = max(max_level, level)
+
+        return sum_level[max_level]
+
+
+
+
+
