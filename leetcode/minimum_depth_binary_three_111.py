51ff22cb8f8a426c71b13d844ec8397a0a046565
antonio Silva <antonios@uoregon.edu>
2023-12-25 08:18:06 -0500

    recursion

---
+++ leetcode/minimum_depth_binary_three_111.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        Q = [(root, 1)]
+
+        while Q:
+            node, level = Q.pop(0)
+            if node:
+                if not node.left and not node.right:
+                    return level
+
+                if node.left:
+                    Q.append((node.left, level + 1))
+                if node.right:
+                    Q.append((node.right, level + 1))
+
+
+
