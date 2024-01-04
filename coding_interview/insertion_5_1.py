3ee919e6edec300ba89fd6f097d02d59d3c2257c
antonio Silva <antonios@uoregon.edu>
2024-01-04 06:30:27 -0800

    recursion

---
+++ coding_interview/insertion_5_1.py
@@ -0,0 +1,5 @@
+i =2
+j =6
+mask = 1 << (j - i + 1)
+mask = ~ mask
+print(bin(mask))
---
+++ leetcode/design/subrectangle_queries_1476.py
@@ -0,0 +1,21 @@
+class SubrectangleQueries:
+
+    def __init__(self, rectangle: List[List[int]]):
+        self.image = copy.deepcopy(rectangle)
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        i = row1
+        while i <= row2:
+            j = col1
+            while j <= col2:
+                self.image[i][j] = newValue
+                j += 1
+            i += 1
+
+    def getValue(self, row: int, col: int) -> int:
+        return self.image[row][col]
+
+# Your SubrectangleQueries object will be instantiated and called as such:
+# obj = SubrectangleQueries(rectangle)
+# obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+# param_2 = obj.getValue(row,col)
