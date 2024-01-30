62b10311984de70697ad1da31df67a27e8ab6c94
antonio Silva <antonios@uoregon.edu>
2024-01-29 20:34:04 -0800

    recursion

---
+++ leetcode/Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_val = max(candies)
+        result = []
+
+        for i in candies:
+            if i + extraCandies >= max_val:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result
