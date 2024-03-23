8e5efcffd0df6e323099bff0f455491314a26d05
antonio Silva <antonios@uoregon.edu>
2024-03-23 06:04:08 -0700

    recursion

---
+++ js/sort_by_2724.js
@@ -0,0 +1,11 @@
+
+//https://leetcode.com/problems/sort-by/?envType=study-plan-v2&envId=30-days-of-javascript
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    arr.sort((a,b)=>fn(a)-fn(b));
+    return arr;
+};
