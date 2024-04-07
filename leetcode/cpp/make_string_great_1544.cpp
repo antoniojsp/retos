1444867c096e2b9fa28d5d22bbee3af1a62c3f21
antonio Silva <antonios@uoregon.edu>
2024-04-07 03:21:53 -0700

    recursion

--- leetcode/cpp/make_string_great_1544.cpp
+++ leetcode/cpp/make_string_great_1544.cpp
@@ -1,24 +1,21 @@
-# https://leetcode.com/problems/make-the-string-great/description/
+
+
+//https://leetcode.com/problems/make-the-string-great/description/
+
 class Solution:
     def makeGood(self, s: str) -> str:
 
-        stack = []
+        stack =  []
 
         for i in range(0, len(s)):
-            if stack and abs(ord(s[i]) - ord(stack[-1])) == ord("a") - ord("A"):
+            if stack and abs(ord(s[i])-ord(stack[-1])) == ord("a")-ord("A"):
                 temp = stack.pop()
                 continue
             else:
                 stack.append(s[i])
 
+
         return "".join(stack)
-
-
-
-
-
-
-
 
 
 
