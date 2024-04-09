146f6e64b4bedd376ad638f9efcf30cbedd8fbaa
antonio Silva <antonios@uoregon.edu>
2024-04-08 19:49:10 -0700

    recursion

---
+++ leetcode/cpp/max_number_of_words_2114.cpp
@@ -0,0 +1,26 @@
+
+//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/description/
+
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+
+        int result = 0;
+
+        for(int i = 0; i<sentences.size(); i++){
+            string current = sentences[i];
+            int count = 0;
+            for(int j = 0; j<current.size(); j++){
+                if(current[j] == ' '){
+                    count++;
+                }
+            }
+            result = count > result? count: result;
+
+        }
+
+
+        return result + 1;
+
+    }
+};
