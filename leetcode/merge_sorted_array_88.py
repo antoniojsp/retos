e5664f659cd4d72086fc5b2203d6cf63b8ff7be7
antonio Silva <antonios@uoregon.edu>
2024-04-09 09:08:09 -0700

    recursion

--- leetcode/merge_sorted_array_88.py
+++ leetcode/merge_sorted_array_88.py
@@ -1,23 +1,12 @@
-from typing import List
-
-
 class Solution:
-
-
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n-1, 0, -1):
-            for j in range(m-1, 0, -1):
-                if nums1[j]
 
+        for i in nums2:
+            nums1[m] = i
+            m += 1
 
-
-
-print(Solution().merge(nums1 = [0], m = 0, nums2 = [1], n = 1))
-
-
-
-
+        nums1.sort()
 
