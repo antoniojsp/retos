5aa287906ca59a14818c14bac523146823fec718
antonio Silva <antonios@uoregon.edu>
2024-04-13 07:51:12 -0700

    recursion

---
+++ leetcode/cpp/substract_the_product_and_sum_1281.cpp
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+
+        int suma = 0;
+        int product = 1;
+        while (1 <= n){
+        suma += n% 10;
+        product *= n % 10;
+        n /= 10;
+        }
+
+    return product - suma;
+    }
+};
