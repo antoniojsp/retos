dc45dab37583e3330b956f995f02496978228b77
Antonio Silva Paucar <antonios@uoregon.edu>
2024-04-17 21:18:58 -0700

    weighted uniform

---
+++ leetcode/remove_linked_list_element_203.py
@@ -0,0 +1,45 @@
+
+
+# https://leetcode.com/problems/remove-linked-list-elements/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        answer = ListNode(-1)
+        dummy = answer
+        current = head
+        while current:
+            if current.val != val:
+                new_node = ListNode(current.val)
+                dummy.next = new_node
+                dummy = dummy.next
+            current = current.next
+
+        return answer.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        answer = ListNode(-1)
+        answer.next = head
+        current = answer
+
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return answer.next
+
+
+
