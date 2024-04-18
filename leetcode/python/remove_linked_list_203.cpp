67dfedd47aec40ac363956952650495bce089ca7
Antonio Silva Paucar <antonios@uoregon.edu>
2024-04-17 21:55:04 -0700

    weighted uniform

---
+++ leetcode/python/remove_linked_list_203.cpp
@@ -0,0 +1,32 @@
+
+//https://leetcode.com/problems/remove-linked-list-elements/description/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy;
+        ListNode* current;
+        dummy.next = head;
+        current = &dummy;
+
+        while(current->next){
+            if(current->next->val == val){
+                current->next = current->next->next;
+            }else{
+                current = current->next;
+            }
+
+        }
+
+        return dummy.next;
+    }
+};
