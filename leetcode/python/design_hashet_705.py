2c78cea7e1fc25ecf907b44f9e7932a32e4648ef
antonio Silva <antonios@uoregon.edu>
2024-04-22 13:30:58 -0700

    recursion

---
+++ leetcode/python/design_hashet_705.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/design-hashset/description/
+class MyHashSet:
+
+    def __init__(self):
+        self.SIZE = 1000
+        self.arr = [[] for i in range(self.SIZE)]
+
+    def add(self, key: int) -> None:
+        key_location = ke y %self.SIZE
+        if key not in self.arr[key_location]:
+            self.arr[key_location].append(key)
+
+
+    def remove(self, key: int) -> None:
+        key_location = ke y %self.SIZE
+        if key in self.arr[key_location]:
+            self.arr[key_location].remove(key)
+
+    def contains(self, key: int) -> bool:
+        key_location = ke y %self.SIZE
+        if key in self.arr[key_location]:
+            return True
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
