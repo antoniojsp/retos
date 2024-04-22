e89f718db92727a4d8c51e0ac42f56ce2fa202c5
antonio Silva <antonios@uoregon.edu>
2024-04-21 20:38:43 -0700

    recursion

--- leetcode/python/design_and_ordered_stream_1656.py
+++ leetcode/python/design_and_ordered_stream_1656.py
@@ -22,3 +22,27 @@ class OrderedStream:
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
 # param_1 = obj.insert(idKey,value)
+
+
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.arr = [""] * (n)  # create a map using a list
+        self.start = 0  # keep track where the self.arr starts
+        self.end = 0
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.arr[idKey - 1] = value
+        for i in range(self.start, len(self.arr)):
+            if self.arr[i] == "":
+                break
+            else:
+                self.end += 1
+
+        answer = self.arr[self.start:self.end]
+        self.start = self.end
+        return answer
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
