1a403e1db71deaaeb20f7744f710cda4fbcc622c
antonio Silva <antonios@uoregon.edu>
2024-04-29 04:42:26 -0700

    recursion

---
+++ leetcode/python/average_level_in_binary_tree_637.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+
+        Q = deque([(root, 0)])
+        values = {}
+        while Q:
+            node, level = Q.popleft()
+            if level not in values:
+                values[level] = (0 ,0)
+            values[level] = (values[level][0 ] +node.val, values[level][1 ] +1)
+
+            if node.left:
+                Q.append((node.left, leve l +1))
+            if node.right:
+                Q.append((node.right, leve l +1))
+
+        return [su m /times for sum, times in values.values()]
+
