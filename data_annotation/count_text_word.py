c0e05fa2fe422bf0dfb5a36e0469f56a9c2ee4d6
antonio Silva <antonios@uoregon.edu>
2024-05-08 20:25:46 -0700

    leetcode

---
+++ data_annotation/count_text_word.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def count_words(file_location:str)->int:
+
+    with open(file_location, "r") as file:
+        answer = file.read().split()
+
+    return len(answer)
+
+def ranking_words(file_location:str)->dict:
+    with open(file_location, "r") as file:
+
+        answer = file.read().split()
+
+    result = list(Counter(answer).items())
+    result.sort(reverse=True, key=lambda x:x[1])
+    return result
+
+if __name__ == "__main__":
+    print(ranking_words("text.txt"))
+
--- leetcode/python/string_comprension_443.py
+++ leetcode/python/string_comprension_443.py
@@ -1,16 +1,53 @@
-# https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
-from typing import List
-
-
 class Solution:
-    def compress(self, chars: List[str]) -> int:
+
+    @staticmethod
+    def precompute_compress_values(chars:list)->list:
+        left = 0
+        right  = left + 1
+        result = []
+        count = 1
+        while right < len(chars):
+            if chars[left] == chars[right]:
+                count+=1
+                right+=1
+            else:
+                result.append((chars[left], count))
+                count= 1
+                left = right
+                right = left +1
+        else:
+            result.append((chars[left], count))
+        return result
+
+
+    @staticmethod
+    def transfer_to_chars_and_count(chars:list, result:list)->int:
+        k = 0
         count = 0
-        temp = ""
-        current = "a"
-        answer = ""
+        for char, times in result:
+            if times == 1:
+                chars[k] = char
+                k+=1
+                count+=1
+            elif times < 10:
+                chars[k], chars[k+1] = char, str(times)
+                k+=2
+                count+=2
+            else:
+                chars[k]=char
+                k+=1
+                count+=1
+                for i in str(times):
+                    chars[k] = i
+                    k += 1
+                    count+=1
+        return count
 
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return 1
 
+        result = self.precompute_compress_values(chars)
+        count = self.transfer_to_chars_and_count(chars, result)
 
-
-
-print(Solution().compress(chars = ["a","a","b","b","c","c","c"]))
+        return count
