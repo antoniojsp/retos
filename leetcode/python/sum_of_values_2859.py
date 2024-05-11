683ae1d90f28e4ae454ab4cbedd16f3e46a35c9c
antonio Silva <antonios@uoregon.edu>
2024-05-11 03:39:04 -0700

    leetcode

--- leetcode/python/sum_of_values_2859.py
+++ leetcode/python/sum_of_values_2859.py
@@ -1,4 +1,7 @@
 # . https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/description/
+from typing import List
+
+
 class Solution:
     def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
         count = 0
@@ -8,6 +11,6 @@ class Solution:
             #     if i == "1":
             #         n_of_ones+=1
             if n_of_ones == k:
-                coun t+ =j
+                count+=j
 
         return count
