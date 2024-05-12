d8f6a052b0991f187f87698260aa9b0cd7ff711f
Antonio Silva Paucar <antonios@uoregon.edu>
2024-05-12 16:16:26 -0700

    weighted uniform

--- leetcode/python/largest_local_values_2373.py
+++ leetcode/python/largest_local_values_2373.py
@@ -1,24 +1,22 @@
-#https://leetcode.com/problems/largest-local-values-in-a-matrix/description/?envType=daily-question&envId=2024-05-12
-from typing import List
-
-
 class Solution:
 
     @staticmethod
-    def check_greatest(grid, row, col ) ->int:
+    def check_greatest_in_grid(grid: list, row: int, col: int) -> int:
+        '''
+        Check row by row from the 3x3 area of the grid for the greatest value.
+        '''
         maximum = 0
-        for i in range(row, row +3):
-            for j in range(col, col +3):
-                maximum = max(maximum, grid[i][j])
+        for i in grid[row:row + 3]:
+            max_of_row = max(i[col:col + 3])
+            maximum = max(maximum, max_of_row)
         return maximum
 
-
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        side_len = len(grid ) -2
+        side_len = len(grid) - 2
         result = [[0 for i in range(side_len)] for i in range(side_len)]
         for row in range(0, side_len):
             for col in range(0, side_len):
-                result[row][col] = self.check_greatest(grid, row, col)
+                result[row][col] = self.check_greatest_in_grid(grid, row, col)
 
         return result
 
