6f45a67b0f55e68ac48db6d7884cb18f40ab0aa8
antonio Silva <antonios@uoregon.edu>
2024-05-14 19:12:02 -0700

    leetcode

---
+++ leetcode/python/find_first_palidrome_arr_2108.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
+class Solution:
+    @staticmethod
+    def check_palindrome(word :str) -> bool:
+
+        left = 0
+        right = len(word ) -1
+
+        while left < right:
+            if word[left] != word[right]:
+                return False
+            left + =1
+            right - =1
+
+        return True
+
+    def firstPalindrome(self, words: List[str]) -> str:
+
+        for word in words:
+            if self.check_palindrome(word):
+                return word
+
+        return ""
