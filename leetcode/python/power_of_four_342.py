63051dec44013a034ecbb35cdda167145489f882
antonio Silva <antonios@uoregon.edu>
2024-05-17 06:03:20 -0700

    leetcode

--- leetcode/python/power_of_four_342.py
+++ leetcode/python/power_of_four_342.py
@@ -3,14 +3,27 @@
 #https://leetcode.com/problems/power-of-four/description/
 
 from math import sqrt, log2
+
+
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n == 1:
-            return True
-        elif n > 0:
-            if n%4 != 0:
-                return False
-            n //= 4
-            temp = log2(n)
-            return temp%2==0
+        if n < 0 or n == 2:
+            return False
+        else:
+            temp = bin(n)[2:]
+            re = sum(list(map(int, temp[1:])))
+
+            # re = temp[1:].count("1")
+            if temp[0] == "1" and len(temp[1:]) % 2 == 0 and re == 0:
+                return True
+
         return False
+        # if n == 1:
+        #     return True
+        # elif n > 0:
+        #     if n%4 != 0:
+        #         return False
+        #     n //= 4
+        #     temp = log2(n)
+        #     return temp%2==0
+        # return False
