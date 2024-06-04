af66dc5eed3da568862788bd323175254a918424
antonio Silva <antonios@uoregon.edu>
2024-06-04 04:39:03 -0700

    leetcode

--- algorithm/dynamic programming/bestSum.py
+++ algorithm/dynamic programming/bestSum.py
@@ -20,4 +20,32 @@ def best_sum(target:int, nums:list):
 
     return shortest
 
+
+def best_sum(target:int, nums:list, memo = None):
+
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+
+    if target == 0:
+        return []
+
+    if target < 0:
+        return None
+
+    shortest = None
+
+    for i in nums:
+        remainder = target - i
+        best = best_sum(remainder, nums, memo)
+        if best is not None:
+            combination  = best + [i]
+            if shortest is None or len(combination) < len(shortest):
+                shortest = combination
+
+    memo[target] = shortest
+    print(memo)
+    return shortest
+
 print(best_sum(8, [1,2,3]))
