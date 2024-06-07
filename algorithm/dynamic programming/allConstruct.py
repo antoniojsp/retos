349679201822650e6af955b6f3d5aea8e7b9abce
Antonio Silva Paucar <antonios@uoregon.edu>
2024-06-06 23:48:32 -0700

    weighted uniform

--- algorithm/dynamic programming/allConstruct.py
+++ algorithm/dynamic programming/allConstruct.py
@@ -9,33 +9,42 @@ def allConstruct(word:str, wordbank:list):
     for i in wordbank:
         if word.startswith(i):
             remainder = word[len(i):]
-            print(remainder)
             ways =  allConstruct(remainder, wordbank)
 
             for way in ways:
                 result.append([i] + way)
 
     return result
+
 print(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
+
+
 class TestAllConstruct(unittest.TestCase):
 
     def test_empty_word(self):
-        self.assertEqual(allConstruct("", ["cat", "dog", "mouse"]), [])
+        self.assertEqual(allConstruct("", ["a", "b", "c"]), [[]])
 
-    def test_single_word_match(self):
-        self.assertEqual(allConstruct("cat", ["cat", "dog", "mouse"]), ["cat"])
+    def test_no_construction(self):
+        self.assertEqual(allConstruct("abc", ["d", "e", "f"]), [])
 
-    def test_multiple_word_match(self):
-        self.assertEqual(allConstruct("catsanddog", ["cat", "cats", "and", "sand", "dog"]), ["cats", "and", "dog"])
+    def test_single_construction(self):
+        self.assertEqual(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]), [['abc', 'def']])
 
-    def test_no_match(self):
-        self.assertEqual(allConstruct("catdog", ["mouse", "elephant", "tiger"]), None)
+    def test_multiple_constructions(self):
+        self.assertEqual(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]),
+                         [['purp', 'le'], ['p', 'ur', 'p', 'le']])
 
-    def test_partial_match(self):
-        self.assertEqual(allConstruct("catdog", ["cat", "elephant", "tiger"]), None)
+    def test_large_input(self):
+        self.assertEqual(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]), [
+            ['enter', 'a', 'p', 'ot', 'ent', 'p', 'ot'],
+            ['enter', 'a', 'p', 'ot', 'ent', 'p', 'o', 't'],
+            ['enter', 'a', 'p', 'o', 't', 'ent', 'p', 'ot'],
+            ['enter', 'a', 'p', 'o', 't', 'ent', 'p', 'o', 't']
+        ])
 
-    def test_full_match(self):
-        self.assertEqual(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]), ["abc", "def"])
+    def test_no_wordbank(self):
+        self.assertEqual(allConstruct("hello", []), [])
 
-# if __name__ == '__main__':
-#     unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
