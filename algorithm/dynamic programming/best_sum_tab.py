fd5de99ddfd3a99938f38440db2a7b13ea6eb550
antonio Silva <antonios@uoregon.edu>
2024-06-08 02:11:05 -0700

    leetcode

--- algorithm/dynamic programming/best_sum_tab.py
+++ algorithm/dynamic programming/best_sum_tab.py
@@ -8,13 +8,27 @@ def best_sum_tab(target, nums):
     for i in range(target+1):
         if table[i] is not None:
             for val in nums:
+                if i + val < len(table):
+                    combo = table[i] + [val]
+                    if table[i+val] is None or len(combo) < len(table[i+val]):
+                        table[i + val] = combo
+
+    return table[-1]
 
 
+import unittest
 
-    print(table)
+class TestBestSumTab(unittest.TestCase):
+    def test_best_sum_tab(self):
+        self.assertEqual(best_sum_tab(7, [5, 3, 4, 7]), [7])
+        self.assertEqual(best_sum_tab(8, [2, 3, 5]), [3, 5])
+        self.assertEqual(best_sum_tab(8, [1, 4, 5]), [4, 4])
+        self.assertEqual(best_sum_tab(100, [1, 2, 5, 25]), [25, 25, 25, 25])
+        self.assertIsNone(best_sum_tab(7, [2, 4]))
+        self.assertEqual(best_sum_tab(0, [1, 2, 3]), [])
 
-
-print(best_sum_tab(8,[2,3,5]))
+if __name__ == '__main__':
+    unittest.main()
 
 # []none none none
 # [2,2,2,2][2,3,3][3,5]u
