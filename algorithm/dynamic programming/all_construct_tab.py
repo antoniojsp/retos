56372020ee0af9a645709ad6e5310f56bd0c4844
Antonio Silva Paucar <antonios@uoregon.edu>
2024-06-09 15:43:36 -0700

    weighted uniform

--- algorithm/dynamic programming/all_construct_tab.py
+++ algorithm/dynamic programming/all_construct_tab.py
@@ -1,20 +1,26 @@
+import unittest
 
 
-def all_construct(word:str, wordbank:list) -> list:
-
-    table = [[]]*(len(word)+1)
+def all_construct(target:str, wordbank:list) -> list:
+    table = [[] for _ in range(len(target) + 1)]
     table[0] = [[]]
 
-    for index, val in enumerate(table):
-        if len(val) > 0:
-            for i in wordbank:
-                if i == word[index:index+len(i)]:
-                    for k, j  in enumerate(table[index]):
-                        table[index+len(i)].append(j)
+    for index, val in enumerate(target):
+        for word in wordbank:
+            if word == target[index:index + len(word)]:
+                temp = []
+                for j in table[index]:
+                    temp.append(j + [word])
+                for k in temp:
+                    table[index + len(word)].append(k)
 
-
-    print(table)
+    return table[-1]
 
 print(all_construct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
+print(all_construct("purple", ["purp", "p", "ur", "le", "purpl"]))
+print(all_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(all_construct("aaaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa", "z"]))
 
-
+target = "abcdef"
+wordbank = ["ab", "abc", "cd", "def", "abcd", "ef", "c"]
+print(all_construct(target, wordbank))
