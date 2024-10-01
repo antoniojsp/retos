11283c712bd30ed2fa59cf6d878e361723884acc
antonio Silva <antonios@uoregon.edu>
2024-10-01 03:18:25 -0700

    leetcode

--- leetcode/python/hash_divide_string_3271.py
+++ leetcode/python/hash_divide_string_3271.py
@@ -1,14 +1,12 @@
 #  https://leetcode.com/problems/hash-divided-string/description/
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
-
-        result = ''
+        start = ord('a')
+        result = []
         for i in range(0 ,len(s), k):
             temp = s[i: i +k]
-            suma = 0
-            for j in temp:
-                suma+=ord(j ) - ord('a')
+            suma = sum([ord(j)-start for j in temp])
 
-            result+=chr((suma%26) + ord('a'))
+            result.append(chr((suma%26) + start))
 
-        return result
+        return "".join(result)
