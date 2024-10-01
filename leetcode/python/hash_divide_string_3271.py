d12eb59f9f90c9a07db5d58f14875e8c867a252e
antonio Silva <antonios@uoregon.edu>
2024-10-01 03:13:07 -0700

    leetcode

--- leetcode/python/hash_divide_string_3271.py
+++ leetcode/python/hash_divide_string_3271.py
@@ -7,8 +7,8 @@ class Solution:
             temp = s[i: i +k]
             suma = 0
             for j in temp:
-                sum a+ =ord(j ) -ord('a')
+                suma+=ord(j ) - ord('a')
 
-            result += chr((sum a %26 ) +ord('a'))
+            result+=chr((suma%26) + ord('a'))
 
         return result
